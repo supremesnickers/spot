a1a0631ca7cfccd4d6eb24f802c10c1dc5736e29
Alexandre Trendel <trendel.alexandre@gmail.com>
2022-01-06 11:21:02 -0500

    start device selector

--- src/app/components/now_playing/now_playing_model.rs
+++ src/app/components/now_playing/now_playing_model.rs
@@ -158,7 +158,7 @@ impl PlaylistModel for NowPlayingModel {
 
 impl SimpleHeaderBarModel for NowPlayingModel {
     fn title(&self) -> Option<String> {
-        Some(gettext("Now playing"))
+        None
     }
 
     fn title_updated(&self, _: &AppEvent) -> bool {
